f3d36bbcc1faddc6918ae1ffd1882582b0bef3ab
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-07-18 18:14:52 -0300

    companies endpoints added

--- src/controllers/companies-controller.ts
+++ src/controllers/companies-controller.ts
@@ -12,4 +12,69 @@ export const companiesController = {
       }
     }
   },
+
+  save: async (req: Request, res: Response) => {
+    const { name, bio, website, email } = req.body;
+
+    try {
+      const companies = await Company.create({
+        name,
+        bio,
+        website,
+        email,
+      });
+      return res.status(201).json(companies);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+
+  show: async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      const company = await Company.findByPk(id);
+      return res.status(200).json(company);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+
+  update: async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name, bio, website, email } = req.body;
+
+    try {
+      const [affectedRows, companies] = await Company.update(
+        { name, bio, website, email },
+        { where: { id }, returning: true }
+      );
+
+      return res.json(companies[0]);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+
+  delete: async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      await Company.destroy({
+        where: { id: id },
+      });
+
+      return res.status(204).send();
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
 };
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { candidatesController } from "./controllers/candidates-controller";
 import { companiesController } from "./controllers/companies-controller";
-import { Candidate } from "./models/candidate";
 
 const router = express.Router();
 
@@ -14,5 +13,9 @@ router.put("/candidates/:id", candidatesController.update);
 router.delete("/candidates/:id", candidatesController.delete);
 
 router.get("/companies", companiesController.index);
+router.get("/companies/:id", companiesController.show);
+router.post("/companies/", companiesController.save);
+router.put("/companies/:id", companiesController.update);
+router.delete("/companies/:id", companiesController.delete);
 
 export { router };
